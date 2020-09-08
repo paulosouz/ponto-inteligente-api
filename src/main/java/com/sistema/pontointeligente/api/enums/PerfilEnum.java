8056b825ff06a8a48bc040136e8d0c00b0b262d4
Sidney Guerra <sidneyguerra@Sidneys-MacBook-Pro.local>
2020-09-07 21:17:37 -0300

    Subida de entities, enums, repositories e services

---
+++ src/main/java/com/sistema/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.sistema.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/java/com/sistema/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,14 @@
+package com.sistema.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sistema.pontointeligente.api.entities.Empresa;
+
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
